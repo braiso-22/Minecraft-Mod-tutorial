1a22f958e0f1770b6bfe73f5af5481e366c2ea15
Brais <braisfv22@gmail.com>
2022-02-22 15:20:54 +0100

    without style, adult and only horse

--- src/com/javaminecraft/SpawnMontura.java
+++ src/com/javaminecraft/SpawnMontura.java
@@ -12,27 +12,30 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpawnMontura extends JavaPlugin {
-    
+
     public static final Logger LOG = Logger.getLogger("Minecraft");
-    
+
     @Override
     public boolean onCommand(CommandSender sender, Command command,
             String label, String[] arguments) {
         if (label.equalsIgnoreCase("spawnmontura")) {
             if (sender instanceof Player) {
-                
+
                 // Lo general cuando se spawnean mobs 
                 Player me = (Player) sender;
                 Location spot = me.getLocation();
                 World world = me.getWorld();
-                
+
                 // Especifico para crear un caballo blanco domesticado
                 Horse horse = world.spawn(spot, Horse.class);
+                horse.setVariant(Horse.Variant.HORSE);
+                horse.setAdult();
+                horse.setStyle(Horse.Style.NONE);
                 horse.setColor(Horse.Color.WHITE);
                 horse.setTamed(true);
                 horse.getInventory().setSaddle(new ItemStack(Material.SADDLE));
                 horse.setOwner(me);
-                
+
                 // Texto que sale por el terminal del server
                 LOG.info("Generado caballo con dueño ".concat(me.getDisplayName()));
                 return true;
